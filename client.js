be7da9c59c07abc771f3d3efc346691c9cea53ea
Sean West <sean.west@generalassemb.ly>
2015-02-19 10:52:39 -0500

    wsurl

---
+++ client.js
@@ -0,0 +1,4 @@
+var WebSocket = require('ws');
+var url = 'ws://localhost:3000/cat/cow';
+var ws = new WebSocket(url);
+
---
+++ server.js
@@ -0,0 +1,9 @@
+var WebSocketServer = require('ws').Server;
+
+var server = new WebSocketServer({port: 3000});
+
+server.on('connection', function(ws) {
+  var url = ws.upgradeReq.url;
+  console.log('client trying to connect to ' + url);
+});
+
